227f2cddb38417f6bb6b3044ea7a1464dd4259ff
athiracev@gmail.com <athiracev@gmail.com>
2023-12-13 11:27:24 +0530

    fading

---
+++ js/loop.js
@@ -0,0 +1,17 @@
+// a=1
+// while(a<5){
+//     console.log("hey!!!")
+//     a++
+// }
+// console.log("\noops loop ended...")
+
+a = 2
+while (a <= 10) {
+
+    console.log(a)
+
+    a = a + 2
+
+
+
+}
