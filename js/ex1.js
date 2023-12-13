34c3a4336e95d5d01299d413e8ad6e21d597d56c
athiracev@gmail.com <athiracev@gmail.com>
2023-12-13 10:57:54 +0530

    fading

---
+++ js/ex1.js
@@ -0,0 +1,52 @@
+a=10;
+b=17;
+c=13;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b)
+// a++
+// console.log(a);
+
+// console.log(a**=2)
+// console.log(a+=2)
+// console.log(a-=2)
+// console.log(a%=2)
+
+// console.log(a!=b)
+// console.log(a==b)
+// console.log(a!==b)
+// console.log(a===b)
+
+// console.log(a>b&&a>c)
+// console.log(a>b||a>c)
+// console.log(!(a>b))
+// console.log(!(a<b))
+
+
+// console.log(a>b?"a is largers":"b is largest")
+
+
+console.log(a+b)
+console.log(10+20)
+console.log("10"+"20")
+console.log(10+"20")
+console.log("10"+20)
+console.log("10"-20)
+console.log("abc"+10)
+console.log("abc"-10)
+console.log("10"*2)
+console.log("10"/2)
+console.log("10"%2)
+console.log(0/2)
+console.log(20/0)
+
+
+
+
+
+
+
