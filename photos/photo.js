7bb0c26a090afa4a4cbaa192d5d44f427a55b9f5
athiracev <athiracev@gmail.com>
2024-01-30 11:03:00 +0530

    last committttt

--- photos/photo.js
+++ photos/photo.js
@@ -1,3 +1,21 @@
-res.fetch("https://jsonplaceholder.typicode.com/photos")
+htmlData=""
+res=fetch("https://jsonplaceholder.typicode.com/photos")
 console.log(res)
-res.then(res => res.json()).then(res => console.log(res)).catch(rej => console.log(rej))
+res.then(res => res.json())
+.then(res =>{
+    data= res.slice(0,50)
+    console.log(data)
+    data.forEach(item=>{
+        htmlData+=`
+        <div class="card" style="width:18rem;">
+            <img class="card-img-top" src="${item.url}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${item.title}</h5>
+            </div>
+        </div>`
+    })
+    console.log(htmlData)
+
+    result.innerHTML=htmlData
+
+}).catch(rej=>console.log(rej))
